34933026cabef11d7555902d4e45475e3ffbc09e
Mert Tecimen <merttecimen@yahoo.com.tr>
2022-06-29 11:08:09 +0300

    Initial Commit

---
+++ Charger Appointer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Charger Appointer
+//
+//  Created by Mert Tecimen on 29.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Charger AppointerUITests/Charger_AppointerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Charger_AppointerUITestsLaunchTests.swift
+//  Charger AppointerUITests
+//
+//  Created by Mert Tecimen on 29.06.2022.
+//
+
+import XCTest
+
+class Charger_AppointerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
